3e4110fbcf014d23d72a3d05c2c0ccf494d2fcb9
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2024-10-23 09:41:33 +0500

    Update layout.tsx
    correct the import line

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
 import Navbar from "./components/nav";
 import { Agdasima, Anybody } from "next/font/google";
-import { Head } from "next/document";
 
 const agdasima = Agdasima({
   subsets: ["latin"],
-  weight: ["400"], // Adjust weight based on the available options
+  weight: ["400"], 
 });
 
 export const metadata: Metadata = {
@@ -20,12 +20,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={agdasima.className}> 
-       <Head>
+    <html lang="en" className={agdasima.className}>
+      <Head>
         <meta property="og:title" content="Muskan's Portfolio" />
         <meta property="og:description" content="Explore my web development projects using Next.js and Tailwind CSS." />
-         <meta property="og:image" content="https://protofolio-2.vercel.app/images/thumbnail.jpg" />
-         <meta property="og:url" content="https://protofilo-2.vercel.app/" />
+        <meta property="og:image" content="https://protofolio-2.vercel.app/images/thumbnail.jpg" />
+        <meta property="og:url" content="https://protofilo-2.vercel.app/" />
         <meta property="og:type" content="website" />
       </Head>
       <body className="bg-gray-200">
