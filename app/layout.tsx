06507c414bf2bbc797475d5cd460c63fcc1c287b
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2024-10-23 10:02:53 +0500

    Update layout.tsx
    remove head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 import "./globals.css";
 import Navbar from "./components/nav";
 import { Agdasima, Anybody } from "next/font/google";
@@ -21,13 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={agdasima.className}>
-      <Head>
-        <meta property="og:title" content="Muskan's Portfolio" />
-        <meta property="og:description" content="Explore my web development projects using Next.js and Tailwind CSS." />
-        <meta property="og:image" content="https://protofolio-2.vercel.app/images/thumbnail.jpg" />
-        <meta property="og:url" content="https://protofilo-2.vercel.app/" />
-        <meta property="og:type" content="website" />
-      </Head>
       <body className="bg-gray-200">
         <Navbar />
         {children}
