61a33e8006f00f08598a00142b600d0ecde07011
Muskan Fatima <160476952+muskan-fatim@users.noreply.github.com>
2024-11-30 11:20:44 +0500

    Update 😀
    add transiton and animation property from line no 9

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,28 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-          cream: '#F9F1D0',
+      keyframes: {
+        'slide-left': {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        'slide-right': {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        'slide-up': {
+          '0%': { transform: 'translateY(50px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        'slide-left': 'slide-left 0.5s ease-out',
+        'slide-right': 'slide-right 0.5s ease-out',
+        'slide-up': 'slide-up 0.5s ease-out',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
